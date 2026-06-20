c457a5f01eca91bbebd41fefa217622ed8948376
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2026-06-20 21:50:57 +0200

    needed libraries and modifications for App testing on mobile

--- mobile-app/lib/core/web_file_picker.dart
+++ mobile-app/lib/core/web_file_picker.dart
@@ -1,23 +1,42 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-import 'dart:async';
+// // ignore: avoid_web_libraries_in_flutter
+// import 'dart:html' as html;
+// import 'dart:async';
+// import 'dart:typed_data';
+
+// Future<(Uint8List, String)?> pickImageFromWeb() async {
+//   final completer = Completer<(Uint8List, String)?>();
+//   final input = html.FileUploadInputElement()..accept = 'image/*';
+//   input.click();
+//   input.onChange.listen((event) async {
+//     final file = input.files?.first;
+//     if (file == null) {
+//       completer.complete(null);
+//       return;
+//     }
+//     final reader = html.FileReader();
+//     reader.readAsArrayBuffer(file);
+//     await reader.onLoad.first;
+//     final bytes = reader.result as List<int>;
+//     completer.complete((Uint8List.fromList(bytes), file.name));
+//   });
+//   return completer.future;
+// }
+
 import 'dart:typed_data';
+import 'package:image_picker/image_picker.dart';
 
 Future<(Uint8List, String)?> pickImageFromWeb() async {
-  final completer = Completer<(Uint8List, String)?>();
-  final input = html.FileUploadInputElement()..accept = 'image/*';
-  input.click();
-  input.onChange.listen((event) async {
-    final file = input.files?.first;
-    if (file == null) {
-      completer.complete(null);
-      return;
-    }
-    final reader = html.FileReader();
-    reader.readAsArrayBuffer(file);
-    await reader.onLoad.first;
-    final bytes = reader.result as List<int>;
-    completer.complete((Uint8List.fromList(bytes), file.name));
-  });
-  return completer.future;
+  final picker = ImagePicker();
+
+  final image = await picker.pickImage(
+    source: ImageSource.gallery,
+  );
+
+  if (image == null) {
+    return null;
+  }
+
+  final bytes = await image.readAsBytes();
+
+  return (bytes, image.name);
 }
